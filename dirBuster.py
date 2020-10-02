937088c9e18b592b71254383b5cc0d2ec4f26724
Ajay-Krishna-A <ajaykrishnaanandhan@gmail.com>
2020-10-02 05:45:32 -0700

    Added dirBuster.py
    DirBuster:
     A bruteforcing tool to view hidden directories and files in a website.
    
    I have made a simple python script which will accept url and wordlist as arguments, and check for directories in the given url using the words in wordlist.
    
    Can be further improved with other features.

---
+++ dirBuster.py
@@ -0,0 +1,95 @@
+# DirBuster
+
+try:
+    # Import required packages
+
+    import sys                  # used to take command line arguments and exit when needed.
+    import requests             # used to make HTTP requests
+
+    # Assign arguments to variables
+
+    rHost = sys.argv[1]         # remote host <url>
+    wordlist = sys.argv[2]      # wordlist <dictionary>
+
+
+
+    # Evaluate the rHost url
+
+    print('Checking for valid url...', end=" ")
+
+    try:
+
+        response = requests.get(rHost).status_code
+
+        if response == 200 :
+
+            print (' Done')
+
+        else :
+
+            print ( "404 - Not found.")
+            sys.exit(1)
+
+    except : 
+
+        print ( "\nError : Cannot reach the rHost ! " )
+        sys.exit(1)
+
+
+    # Parsing the wordlist 
+
+    try :
+
+        print("Parsing the wordlist ... ", end=" ")
+
+        with open(wordlist) as file :
+
+            to_check = file.read().strip().split('\n')
+
+            print ( " Done " )
+            print ( "Total Paths to check : ", str(len(to_check)) )
+
+
+    except IOError :
+        
+        print('[!] Failed...')
+        print('Error : Failed to read Specified file - Wordlist')
+        sys.exit(1)
+
+    # checking for the path : function defined.
+        
+    def checkpath(path):
+        
+        try : 
+            
+            response = requests.get(rHost + '/' + path ).status_code
+
+            if ( response == 200 ):
+
+                print('[*] Valid path found : ', path)
+            
+
+                
+        
+        except Exception:
+            
+            print('[!] Failed...')   
+            print('Error : An unexpected error occured ...') 
+            sys.exit(1)
+            
+      #  iterating over the list of paths...
+
+    print('\n\n Beginning scan .... ')
+    
+    for i in range(len(to_check)) :
+        checkpath(to_check[i])
+        
+    print('\n Scan complete ...') 
+
+
+
+
+except KeyboardInterrupt :
+    
+    print('Error : User interrupted scan ... ')
+    sys.exit(1)
